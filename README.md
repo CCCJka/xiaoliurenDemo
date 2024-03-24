cff27ee098a1af985df8bece6ffd8a2189ff167f
aliyun0747856956 <2534153271@qq.com>
2024-03-24 23:42:35 +0800

    updateDemo

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-# xiaoliurenDemo
 日历加上小六爻的基础推算功能
 
 ```
 API接口：
 农历：https://www.36jxs.com/api/Commonweal/almanac?sun=2024-3-21  sun为申请的国历
 ```
+
+
+
+更新历史：
+
+2024-3-24：粗略写了一个Demo，判断当前日月时并写显示结语
--- app/src/main/java/com/cccjka/liuyao/utils/DateUtils.kt
+++ app/src/main/java/com/cccjka/liuyao/utils/DateUtils.kt
@@ -1,24 +1,10 @@
 package com.cccjka.liuyao.utils
 
 import java.text.SimpleDateFormat
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 
 object DateUtils {
 
-    fun getMonth(): Int {
-        val df = SimpleDateFormat("MM") //返回月份
-        val num = df.format(Date()).toInt()
-        return num
-    }
-
-    fun getDay(): Int {
-        val df = SimpleDateFormat("dd") //返回当天为几号
-        val num = df.format(Date()).toInt()
-        return num
-    }
-
-    fun getHours(): Int {
-        val df = SimpleDateFormat("HH") //返回当前小时
-        return df.format(Date()).toInt()
-    }
 }
